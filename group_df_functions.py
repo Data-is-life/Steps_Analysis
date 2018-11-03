721da726ad97d7f8345c0f408efefc0b9c33738c
data-is-life <mohitgangwani@outlook.com>
2018-11-03 10:09:27 -0700

    new functions with comments and grouping

--- group_df_functions.py
+++ group_df_functions.py
@@ -5,45 +5,19 @@
 import pandas as pd
 import numpy as np
 
-
-
-def create_month_df(df):
-	pass
-def create_weekley_df(df):
-	pass
-def create_seven_day_rolling_df(df):
-	pass
-def create_thirty_day_rolling_df(df):
-	pass
-
-
-
-
-
+def remove_end_date_rename_start_date(df):
+	df.drop(columns=['end_date'], inplace=True)
+	df.rename({'start_date': 'date'}, inplace=True)
+	return df
 
 def sum_day_df(df):
-    summed_df = df.groupby(by=['start_date']).sum()
+	df = remove_end_date_rename_start_date(df)
+    summed_df = df.groupby(by=['date']).sum()
     summed_df.reset_index(inplace=True)
     summed_df['steps_per_hour'] = summed_df[
         'num_steps'] / summed_df['duration']
     summed_df.drop(columns=['start_time', 'end_time'], inplace=True)
     return summed_df
-
-
-def mean_day_df(df):
-    mean_df = df.groupby(by=['start_date']).mean()
-    mean_df.reset_index(inplace=True)
-    mean_df['steps_per_hour'] = mean_df[
-        'num_steps'] / mean_df['duration']
-    return mean_df
-
-
-def median_day_df(df):
-    median_df = df.groupby(by=['start_date']).median()
-    median_df.reset_index(inplace=True)
-    median_df['steps_per_hour'] = median_df[
-        'num_steps'] / median_df['duration']
-    return median_df
 
 
 def sum_month_df(df):
@@ -79,6 +53,14 @@ def sum_week_df(df):
     summed_df.drop(columns=['start_time', 'end_time'])
     return summed_df
 
+def sum_week_df(df):
+	summed_df = df.groupby(by=['start_date']).sum()
+    summed_df.reset_index(inplace=True)
+    summed_df['steps_per_hour'] = summed_df[
+        'num_steps'] / summed_df['duration']
+    summed_df.drop(columns=['start_time', 'end_time'])
+    return summed_df
+
 
 def mean_week_df(df):
 	mean_df = df.groupby(by=['start_date']).mean()
@@ -95,3 +77,12 @@ def median_week_df(df):
     median_df['steps_per_hour'] = median_df[
         'num_steps'] / median_df['duration']
     return median_df
+
+def create_month_df(df):
+	pass
+def create_weekley_df(df):
+	pass
+def create_seven_day_rolling_df(df):
+	pass
+def create_thirty_day_rolling_df(df):
+	pass
